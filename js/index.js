bce3fa0d5f309ee5a20edaf38f420318d4aabdb8
Emre Tanirgan <emretanirgan@hotmail.com>
2013-01-19 16:41:48 -0500

    Added some Dropbox stuff

--- js/index.js
+++ js/index.js
@@ -36,6 +36,13 @@ var gpsFunctions = {
        'message: ' + error.message + '\n');*/
   }
 }
+client.readFile("hello_world.txt", function(error, data) {
+  if (error) {
+    return showError(error);  // Something went wrong.
+  }
+
+  alert(data);  // data has the file's contents
+});
 var app = {
     // Application Constructor
     initialize: function() {
@@ -55,6 +62,39 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        var Dropbox = require("dropbox");
+        var client = new Dropbox.Client({
+          key: "sw0vbtsn4svk086", secret: "zgwynjp4f7h1qku", sandbox:true
+        });
+
+        client.authDriver(new Dropbox.Drivers.Redirect());
+        /*client.authDriver(new Dropbox.Drivers.NodeServer(8191));*/
+        client.authenticate(function(error, client) {
+        if (error) {
+        // Replace with a call to your own error-handling code.
+        //
+        // Don't forget to return from the callback, so you don't execute the code
+        // that assumes everything went well.
+          return showError(error);
+        }
+        client.onError.addListener(function(error) {
+          if (window.console) {  // Skip the "if" in node.js code.
+            console.error(error);
+          }
+        });
+
+        // Replace with a call to your own application code.
+        //
+        // The user authorized your app, and everything went well.
+        // client is a Dropbox.Client instance that you can use to make API calls.
+        dclient.readFile("msgbottle.txt", function(error, data) {
+          if (error) {
+            return showError(error);  // Something went wrong.
+          }
+
+          alert(data);  // data has the file's contents
+        });
+      });
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -72,4 +112,36 @@ var app = {
         navigator.geolocation.getCurrentPosition(gpsFunctions.onSuccess, gpsFunctions.onError);
     }
     
+};
+
+var showError = function(error) {
+  switch (error.status) {
+    case 401:
+    // If you're using dropbox.js, the only cause behind this error is that
+    // the user token expired.
+    // Get the user through the authentication flow again.
+    break;
+
+    case 404:
+    // The file or folder you tried to access is not in the user's Dropbox.
+    // Handling this error is specific to your application.
+    break;
+
+    case 507:
+    // The user is over their Dropbox quota.
+    // Tell them their Dropbox is full. Refreshing the page won't help.
+    break;
+
+    case 503:
+    // Too many API requests. Tell the user to try again later.
+    // Long-term, optimize your code to use fewer API calls.
+    break;
+
+    case 400:  // Bad input parameter
+    case 403:  // Bad OAuth request.
+    case 405:  // Request method not expected
+    default:
+      // Caused by a bug in dropbox.js, in your application, or in Dropbox.
+      // Tell the user an error occurred, ask them to refresh the page.
+  }
 };
